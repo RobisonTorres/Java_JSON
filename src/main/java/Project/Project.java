56704decdcb5dde203946dff581e748b34257497
RobisonTorres <robison.torres21@gmail.com>
2024-08-15 18:58:51 -0300

    updated code.

--- src/main/java/Project/Project.java
+++ src/main/java/Project/Project.java
@@ -6,10 +6,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.commons.lang3.StringUtils;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Project {
 
@@ -23,69 +20,76 @@ public class Project {
     public static void saveChanges(JsonNode rootNode) throws IOException{
 
         // This function saves alterations made in the stockProducts.json.
-        objectMapper.writerWithDefaultPrettyPrinter().writeValue(file, rootNode);
+        objectMapper.writerWithDefaultPrettyPrinter(). writeValue(file, rootNode);
     }
-    public static ArrayList<String> allProducts(JsonNode rootNode){
+    public static ArrayList<String> allProducts() throws IOException{
 
         // This function returns all the products present in the stockProducts.json.
+        JsonNode rootNode = rootNode();
         ArrayList<String> allProducts = new ArrayList<>();
         Iterator<String> iterator = rootNode.fieldNames();
         iterator.forEachRemaining(allProducts::add);
         return allProducts;
     }
+    public static String testUserInput(String productName){
+
+        // This function tests if the user input is valid.
+        // Price should be greater than $0 and quantity greater or equals to 0.
+        try {
+            JsonNode rootNode = rootNode();
+            Scanner userInput = new Scanner(System.in);
+            System.out.print("Please, type in the price and quantity, separated by space: \n");
+            Double productPrice = Double.parseDouble(userInput.next());
+            int productQuantity = userInput.nextInt();
+            if (productName.matches("[a-zA-Z]+") && productPrice > 0 && productQuantity >= 0){
+                ArrayNode priceQuantity = objectMapper.createArrayNode();
+                priceQuantity.add(productPrice);
+                priceQuantity.add(productQuantity);
+                ((ObjectNode) rootNode).set(productName, priceQuantity);
+                saveChanges(rootNode);
+                return "Success! " + productName + " has been added or update in the stock.";
+                } else {
+                    return "Error! Invalid name, price or quantity.";
+                }
+            } catch (Exception e) {
+                return "Error! Invalid input.";
+            }
+        }
     public static String createProduct() throws IOException {
 
-        // This function creates a product into the stockProducts.json based on the user input.
-        JsonNode rootNode = rootNode();
+        // This function adds a new product into the stockProducts.json based on the user input.
         Scanner userInput = new Scanner(System.in);
-        System.out.print("Please, type in the name, price and quantity to add a new product to the stock: \n");
-        String nameProduct = StringUtils.capitalize(userInput.next().toLowerCase());
-
-        if (allProducts(rootNode).contains(nameProduct)) {
-            return "Error! " + nameProduct + " is already present in the stock.";
+        System.out.print("Please, type in the name to add a new product to the stock: \n");
+        String productName = StringUtils.capitalize(userInput.next().toLowerCase());
+        if (allProducts().contains(productName)) {
+            return "Error! " + productName + " is already present in the stock.";
         } else {
-            Double priceProduct = Double.parseDouble(userInput.next());
-            int quantityProduct = userInput.nextInt();
-            ArrayNode priceQuantity = objectMapper.createArrayNode();
-            priceQuantity.add(priceProduct);
-            priceQuantity.add(quantityProduct);
-            ((ObjectNode) rootNode).set(nameProduct, priceQuantity);
-            saveChanges(rootNode);
-            return "Success! " + nameProduct + " has been added to the stock.";
+            return testUserInput(productName);
         }
     }
     public static void readProducts() throws IOException {
 
         // This function reads and displays all the products present in stockProducts.json.
         JsonNode rootNode = rootNode();
-        System.out.println("Product" + "\t" + "Price" + "\t" + "Quantity");
+        System.out.format("%-15s%-10s%-10s%n", "Product", "Price", "Quantity");
         Iterator<Map.Entry<String, JsonNode>> fields = rootNode.fields();
         while (fields.hasNext()) {
             Map.Entry<String, JsonNode> field = fields.next();
             String key = field.getKey();
             JsonNode item = field.getValue();
-            System.out.println(key + "\t" + item.get(0).asText() + "\t" + item.get(1).asText());
+            System.out.format("%-15s%-10s%-10s%n", key, item.get(0).asText(), item.get(1).asText());
         }
     }
     public static String updateProduct() throws IOException {
 
-        // This function updates product present in the stockProducts file based on the user input.
-        JsonNode rootNode = rootNode();
+        // This function updates product's price and quantity present in the stockProducts file based on the user input.
         Scanner userInput = new Scanner(System.in);
-        System.out.print("Please, type in the name, price and quantity to update a product: \n");
-        String nameProduct = StringUtils.capitalize(userInput.next().toLowerCase());
-
-        if (allProducts(rootNode).contains(nameProduct)) {
-            Double priceProduct = Double.parseDouble(userInput.next());
-            int quantityProduct = userInput.nextInt();
-            ArrayNode priceQuantity = objectMapper.createArrayNode();
-            priceQuantity.add(priceProduct);
-            priceQuantity.add(quantityProduct);
-            ((ObjectNode) rootNode).set(nameProduct, priceQuantity);
-            saveChanges(rootNode);
-            return "Success! " + nameProduct + " has been updated.";
+        System.out.print("Please, type in the name to update a product: \n");
+        String productName = StringUtils.capitalize(userInput.next().toLowerCase());
+        if (allProducts().contains(productName)) {
+            return testUserInput(productName);
         } else {
-            return "Error!" + nameProduct + " is not present in the stock.";
+            return "Error!" + productName + " is not present in the stock.";
         }
     }
     public static String deleteProduct() throws IOException{
@@ -95,7 +99,7 @@ public class Project {
         Scanner userInput = new Scanner(System.in);
         System.out.print("Enter the product you would like to delete: \n");
         String deleteProduct = StringUtils.capitalize(userInput.next().toLowerCase());
-        if(allProducts(rootNode).contains(deleteProduct)) {
+        if(allProducts().contains(deleteProduct)) {
             ((ObjectNode) rootNode).remove(deleteProduct);
             saveChanges(rootNode);
             return "Success! " + deleteProduct + " has been deleted.";
@@ -105,10 +109,32 @@ public class Project {
     }
     public static void main(String[] args) throws IOException {
 
-        System.out.println(createProduct());
-        readProducts();
-        System.out.println(updateProduct());
-        System.out.println(deleteProduct());
-        System.out.println("Test commit.");
+        System.out.println("Menu options:\n");
+        System.out.println("1. Add new product");
+        System.out.println("2. Update a product");
+        System.out.println("3. Show all products");
+        System.out.println("4. Delete a product");
+        System.out.println("5. Exit the program\n");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your option: ");
+        String inputOption = input.next();
+        System.out.println();
+        switch (inputOption) {
+            case "1":
+                System.out.println(createProduct());
+                break;
+            case "2":
+                System.out.println(updateProduct());
+                break;
+            case "3":
+                readProducts();
+                break;
+            case "4":
+                System.out.println(deleteProduct());
+            case "5":
+                break;
+            default:
+                System.out.println("There no match.");
+        }
     }
 }
